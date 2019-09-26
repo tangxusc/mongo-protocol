9c9897316ea575dd88ee43d9ebfd9cfdf4ec936e
tangxu <562050688@qq.com>
2019-09-26 17:27:09 +0800

    实现基础的query命令

--- server_test.go
+++ server_test.go
@@ -45,14 +45,168 @@ func (t *TestHandler) Process(header *MsgHeader, r *Reader, w io.Writer) error {
 	fmt.Println(string(bytes))
 	logrus.Debugf(`%s`, bytes)
 
+	_, ok := query.Query["$query"]
+	if ok {
+		return listDatabase(query, w)
+	}
+
+	_, ok = query.Query["isMaster"]
+	if ok {
+		return isMaster(query, w)
+	}
+
+	_, ok = query.Query["whatsmyuri"]
+	if ok {
+		return whatsmyuri(query, w)
+	}
+
+	_, ok = query.Query["buildinfo"]
+	if ok {
+		return buildinfo(query, w)
+	}
+	_, ok = query.Query["buildInfo"]
+	if ok {
+		return buildinfo(query, w)
+	}
+
+	_, ok = query.Query["serverStatus"]
+	if ok {
+		return serverStatus(query, w)
+	}
+
+	_, ok = query.Query["serverStatus"]
+	if ok {
+		return serverStatus(query, w)
+	}
+
+	return defaultReply(query, w)
+}
+
+func defaultReply(query *Query, w io.Writer) error {
 	reply := NewReply(query.Header.RequestID)
 	reply.NumberReturned = 1
-	reply.Documents = map[string]interface{}{"isMaster": 1, "ok": 1}
+	reply.Documents = map[string]interface{}{"ok": 1}
 
-	e = reply.Write(w)
+	e := reply.Write(w)
 	if e != nil {
 		return e
 	}
 
+	return nil
+}
+
+func serverStatus(query *Query, w io.Writer) error {
+	reply := NewReply(query.Header.RequestID)
+	reply.NumberReturned = 1
+	reply.Documents = map[string]interface{}{"you": "118.114.245.36:48780", "ok": 1}
+
+	e := reply.Write(w)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func buildinfo(query *Query, w io.Writer) error {
+	reply := NewReply(query.Header.RequestID)
+	reply.NumberReturned = 1
+	reply.Documents = map[string]interface{}{
+		"version":          "4.2.0",
+		"gitVersion":       "a4b751dcf51dd249c5865812b390cfd1c0129c30",
+		"modules":          make([]string, 0),
+		"allocator":        "tcmalloc",
+		"javascriptEngine": "mozjs",
+		"sysInfo":          "deprecated",
+		"versionArray":     [4]int32{4, 2, 0, 0},
+		"openssl": map[string]interface{}{
+			"running":  "OpenSSL 1.1.1  11 Sep 2018",
+			"compiled": "OpenSSL 1.1.1  11 Sep 2018",
+		},
+		"buildEnvironment": map[string]interface{}{
+			"distmod":     "ubuntu1804",
+			"distarch":    "x86_64",
+			"cc":          "/opt/mongodbtoolchain/v3/bin/gcc: gcc (GCC) 8.2.0",
+			"ccflags":     "-fno-omit-frame-pointer -fno-strict-aliasing -ggdb -pthread -Wall -Wsign-compare -Wno-unknown-pragmas -Winvalid-pch -Werror -O2 -Wno-unused-local-typedefs -Wno-unused-function -Wno-deprecated-declarations -Wno-unused-const-variable -Wno-unused-but-set-variable -Wno-missing-braces -fstack-protector-strong -fno-builtin-memcmp",
+			"cxx":         "/opt/mongodbtoolchain/v3/bin/g++: g++ (GCC) 8.2.0",
+			"cxxflags":    "-Woverloaded-virtual -Wno-maybe-uninitialized -fsized-deallocation -std=c++17",
+			"linkflags":   "-pthread -Wl,-z,now -rdynamic -Wl,--fatal-warnings -fstack-protector-strong -fuse-ld=gold -Wl,--build-id -Wl,--hash-style=gnu -Wl,-z,noexecstack -Wl,--warn-execstack -Wl,-z,relro",
+			"target_arch": "x86_64",
+			"target_os":   "linux",
+		},
+		"bits":              64,
+		"debug":             false,
+		"maxBsonObjectSize": 16777216,
+		"storageEngines": [4]string{
+			"biggie",
+			"devnull",
+			"ephemeralForTest",
+			"wiredTiger",
+		},
+		"ok": 1,
+	}
+
+	e := reply.Write(w)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func whatsmyuri(query *Query, w io.Writer) error {
+	reply := NewReply(query.Header.RequestID)
+	reply.NumberReturned = 1
+	reply.Documents = map[string]interface{}{"you": "118.114.245.36:48780", "ok": 1}
+
+	e := reply.Write(w)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func isMaster(query *Query, writer io.Writer) error {
+	reply := NewReply(query.Header.RequestID)
+	reply.NumberReturned = 1
+	reply.Documents = map[string]interface{}{"isMaster": 1, "ok": 1}
+
+	e := reply.Write(writer)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func listDatabase(query *Query, w io.Writer) error {
+	reply := NewReply(query.Header.RequestID)
+	reply.NumberReturned = 1
+	reply.Documents = map[string]interface{}{
+		"totalSize": 274432,
+		"ok":        1,
+		"databases": []interface{}{
+			map[string]interface{}{
+				"name":       "admin",
+				"sizeOnDisk": 102400,
+				"empty":      false,
+			},
+			map[string]interface{}{
+				"name":       "config",
+				"sizeOnDisk": 98304,
+				"empty":      false,
+			},
+			map[string]interface{}{
+				"name":       "local",
+				"sizeOnDisk": 73728,
+				"empty":      false,
+			},
+		},
+	}
+	e := reply.Write(w)
+	if e != nil {
+		return e
+	}
 	return nil
 }
